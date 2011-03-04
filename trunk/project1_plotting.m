bd5e91c8d999d4e63a4bedf64e492d6e804bc7df
karl.fetzer <karl.fetzer@4884e3fd-9cd5-8525-4b12-2cabdc75178c>
2011-03-04 01:40:41 +0000

    Project 1, third draft.

--- trunk/project1_plotting.m
+++ trunk/project1_plotting.m
@@ -1,29 +1,42 @@
-function [] = project1_plotting(time,w_n,w,quat_n,theta,thetaDot,C_NtoB)
-% This function plots the results of Project 1
+function [] = project1_plotting(time,w_n,w,C_NtoB,theta,thetaDot,quat_n)
+%This function plots the results of Project 1
 % Assumptions:
 % Author: Karl Ludwig Fetzer
 
+
+scrsz = get(0,'ScreenSize');
+% Example
+%figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+% ScreenSize = [left, bottom, width, height]
+%where left and bottom define the distance from the lower-left corner of
+%the screen to the lower-left corner of the figure window. width and height define the dimensions of the window.
+
 %% Angular velocity
-figure
-plot(time,w_n(1,:),'b-',time,w_n(2,:),'b-.',time,w_n(3,:),'b.')
+figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+plot(time,w_n(1,:),'b-',time,w_n(2,:),'k-',time,w_n(3,:),'r-')
 hold on
-plot(time,w(1,:),'k-', time,w(2,:),'k-.', time,w(3,:),'k.')
+plot(time,w(1,:),'b.', time,w(2,:),'k.', time,w(3,:),'r.')
 title('Angular velocity time history')
 ylabel('angular velocity (rad/s)')
 xlabel('time (s)')
+legend('w_1 numerical','w_2 numerical','w_3 numerical','w_1 analytical','w_2 analytical','w_3 analytical','Location','EastOutside');
+%legend('boxoff'); % Makes legend transparent
+savefig('output/w_vs_t',gcf,'eps');
 
 %% Euler Angle Sets
-figure
+figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
 plot(time,theta(1,:))
 title('Precession angle time history')
 ylabel('precession angle (degrees)')
 xlabel('time (s)')
+savefig('output/precession',gcf,'eps');
 
-figure
+figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
 plot(time,theta(2,:))
 title('Nutation angle time history')
 ylabel('nutation angle (degrees)')
 xlabel('time (s)')
+savefig('output/nutation',gcf,'eps');
 
 % figure
 % plot(time,theta(1,:), time,theta(2,:), time,theta(3,:))
@@ -37,22 +50,23 @@ C32 = zeros(1,length(time));
 C12(:) = C_NtoB(1,2,:);
 C32(:) = C_NtoB(3,2,:);
 
-figure
+figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
 DCMplot = plot(C12,C32);
 title('C32 vs C12')
 ylabel('C32')
 xlabel('C12')
-
+savefig('output/C12vsC32',gcf,'eps');
 
 for (index = 1: length(time))
    if (time(index) == 1.5 || time(index) == 3.1)
-      figure
+      figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
       DCMplot = plot(C12,C32);
-      txt = strcat('C32 vs C12 with label at t=', num2str(time(index)),'sec')
+      txt = strcat('C32 vs C12 with label at t=', num2str(time(index)),'sec');
       title(txt)
       ylabel('C32')
       xlabel('C12')
       makedatatip(DCMplot,index)
+      savefig(strcat('output/C12vsC32_at', num2str(index)),gcf,'eps');
    end
 end
 text
