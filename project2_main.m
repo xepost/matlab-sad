769b5ed5fafb7ebb09811bc93a6dfff15685827f
karl.fetzer <karl.fetzer@4884e3fd-9cd5-8525-4b12-2cabdc75178c>
2011-04-20 11:39:41 +0000

    Project 2, Task C, part a - index according to multiple s_star values.

--- project2_main.m
+++ project2_main.m
@@ -16,7 +16,7 @@ I = 1200;    % kg-m^2
 J = 200;     % kg-m^2
 K = (I-J)/I; % dimensionless
 
-orbit_step      = 0.05; % rev
+orbit_step      = 0.001; % rev
 orbits_complete = 4;  % rev
 
 % For b2 as axis of symmetry for axisymmetric body B:
@@ -24,14 +24,15 @@ I_tensor = [I 0 0;
             0 J 0; 
             0 0 I];
          
-n = [-25,-5,5,25];
+n = [20];
+
+s_star = [0 K*n n-1];
 
 psi_0 = 7; % degrees
 
-for n_index = 1:length(n)
-   s_star = n(n_index) - 1;
+for s_index = 1:length(s_star)
    %Initial conditions:
-   w0 = [0 n(n_index) 0]; % rad/s
+   w0 = [0 n 0]; % rad/s
    E0 = [sind(psi_0/2) 0 0 cosd(psi_0/2)];
    % % 
    % % H0 = dyadicRightDot(I_tensor,w0);
@@ -45,25 +46,25 @@ for n_index = 1:length(n)
    % http://www.mathworks.com/matlabcentral/newsreader/view_thread/242818
    % Implementation is found here:
    % http://www.facstaff.bucknell.edu/maneval/help211/usingode45.pdf
-   fileHandle = @(nu,V) project2_ODE(nu,V,s_star,K);
+   fileHandle = @(nu,V) project2_ODE(nu,V,s_star(s_index),K);
 
    [nu,V] = ode45(fileHandle, [0:orbit_step:orbits_complete],[w0 E0]);
    
-   orbits(n_index,:)         = nu;
-   state_vector(n_index,:,:) = V;
-   state_vector_errors(n_index,:,1) = V(:,1) - 0;
-   state_vector_errors(n_index,:,2) = V(:,2) - n(n_index);
-   state_vector_errors(n_index,:,3) = V(:,3) - 0;
-   state_vector_errors(n_index,:,4) = V(:,4) - 0;
-   state_vector_errors(n_index,:,5) = V(:,5) - 0;
-   state_vector_errors(n_index,:,6) = V(:,6) - 0;
-   state_vector_errors(n_index,:,7) = V(:,7) - 1;
+   orbits(s_index,:)         = nu;
+   state_vector(s_index,:,:) = V;
+   state_vector_errors(s_index,:,1) = V(:,1) - 0;
+   state_vector_errors(s_index,:,2) = V(:,2) - n;
+   state_vector_errors(s_index,:,3) = V(:,3) - 0;
+   state_vector_errors(s_index,:,4) = V(:,4) - 0;
+   state_vector_errors(s_index,:,5) = V(:,5) - 0;
+   state_vector_errors(s_index,:,6) = V(:,6) - 0;
+   state_vector_errors(s_index,:,7) = V(:,7) - 1;
    
 end
 
 c22 = 1 - 2.*state_vector(:,:,6).^2 - 2.*state_vector(:,:,4).^2;
 psi = acosd(c22);
    
-project2_plotting(nu,state_vector,n, psi)
+project2_plotting(nu,state_vector,s_star, psi)
 
 end
