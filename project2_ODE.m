aeb606c65960bc80ff7f26eae852f67bcd12ca40
karl.fetzer <karl.fetzer@4884e3fd-9cd5-8525-4b12-2cabdc75178c>
2011-04-21 02:59:47 +0000

    Project 2, Task C, part b complete

--- project2_ODE.m
+++ project2_ODE.m
@@ -15,9 +15,9 @@ dv(2) = 0;
 dv(3) = 2*pi* (-s_star * v(1) + K * (-12*(v(6)*v(4)-v(5)*v(7))*(v(5)*v(6)+v(4)*v(7)) + v(1)*v(2) ));
 
 % Kinematic Differential vquations
-dv(4) =  pi * ( v(1)*v(7) - v(2)*v(6) + v(3)*v(5) + v(6) + s_star*v(6) );
+dv(4) =  pi * ( v(1)*v(7) - v(2)*v(6) + v(3)*v(5) - v(6) + s_star*v(6) );
 dv(5) =  pi * ( v(1)*v(6) + v(2)*v(7) - v(3)*v(4) - v(7) - s_star*v(7) );
-dv(6) =  pi * (-v(1)*v(5) + v(2)*v(4) + v(3)*v(7) - v(4) - s_star*v(4) );
+dv(6) =  pi * (-v(1)*v(5) + v(2)*v(4) + v(3)*v(7) + v(4) - s_star*v(4) );
 dv(7) =  pi * (-v(1)*v(4) - v(2)*v(5) - v(3)*v(6) + v(5) + s_star*v(5) );
 
 end
