4d0955502e223dc137006377f1580e2fe68fe0cf
karl.fetzer <karl.fetzer@4884e3fd-9cd5-8525-4b12-2cabdc75178c>
2011-04-21 12:21:18 +0000

    Project 2, Task C, part c complete.

--- project2_plotting.m
+++ project2_plotting.m
@@ -1,24 +1,119 @@
-function [] = project2_plotting(nu,state_vector,n,psi1,psi2,psi3)
+function [] = project2_plotting(nu,state_vector,n,psi, zeta, eta, phi)
 %This function plots the results of Project 1
 % Assumptions:
 % Author: Karl Ludwig Fetzer
 
 scrsz = get(0,'ScreenSize');
-% % 
+% % % for n_index = 1:length(n)
+%    if (n_index == 1)
+%       figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+% %       axes('YLim',[-5 25])
+%       title('Psi vs orbit')
+%       ylabel('Psi (degrees)')
+%       xlabel('Orbits (rev)')
+%       hold on
+%       color = 'k.-';
+%    elseif (n_index == 2)
+%       color = 'k*-';
+%    elseif (n_index == 3)
+%       color = 'b*-';
+%    elseif (n_index == 4)
+%       color = 'b.-';      
+%    end
+%    plot(nu,psi(n_index,:),color)
+% end
+% savefig('output/proj2_psi',gcf,'pdf');
+
 %% Task B:
 %% Nutation Angle
 for n_index = 1:length(n)
-%  for n_index = 1
-   figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
-%       axes('YLim',[-5 25])
-   title(strcat('Psi vs orbit for n = ', num2str(n(n_index))))
-   ylabel('Psi (degrees)')
-   xlabel('Orbits (rev)')
-   hold on
-   plot(nu,psi1(n_index,:),'k.-',nu,psi2(n_index,:),'b.-',nu,psi3(n_index,:),'r.-')
-   hold off
-   savefig(strcat('output/proj2_psi_n_eq_', num2str(n(n_index))),gcf,'pdf');
+   if (n_index == 1)
+      figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+   %       axes('YLim',[-5 25])
+      title('Psi vs orbit ')
+      ylabel('Psi (degrees)')
+      xlabel('Orbits (rev)')
+      hold on
+      color = 'k.-';
+   elseif (n_index == 2)
+      color = 'r.-';
+   elseif (n_index == 3)
+      color = 'b.-';
+   elseif (n_index == 4)
+      color = 'g.-';    
+   end
+      plot(nu,psi(n_index,:),color)
 end
+hold off
+savefig('output/proj2_psi',gcf,'pdf');
+
+%% Orbit Angle
+for n_index = 1:length(n)
+   if (n_index == 1)
+      figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+   %       axes('YLim',[-5 25])
+      title('Zeta vs orbit ')
+      ylabel('Zeta (degrees)')
+      xlabel('Orbits (rev)')
+      hold on
+      color = 'k.-';
+   elseif (n_index == 2)
+      color = 'r.-';
+   elseif (n_index == 3)
+      color = 'b.-';
+   elseif (n_index == 4)
+      color = 'g.-';    
+   end
+      plot(nu,zeta,color)
+end
+   hold off
+   savefig('output/proj2_zeta',gcf,'pdf');
+   
+%% Precession Angle
+for n_index = 1:length(n)
+   if (n_index == 1)
+      figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+   %       axes('YLim',[-5 25])
+      title('Eta vs orbit ')
+      ylabel('Eta (degrees)')
+      xlabel('Orbits (rev)')
+      hold on
+      color = 'k.-';
+   elseif (n_index == 2)
+      color = 'r.-';
+   elseif (n_index == 3)
+      color = 'b.-';
+   elseif (n_index == 4)
+      color = 'g.-';    
+   end
+      plot(nu,eta(n_index,:),color)
+end
+   hold off
+   savefig('output/proj2_eta',gcf,'pdf');
+   
+   
+%% Phi Angle
+for n_index = 1:length(n)
+   if (n_index == 1)
+      figure('Position',[scrsz(3)/4 scrsz(4)/2 scrsz(3)/2 scrsz(4)/2])
+   %       axes('YLim',[-5 25])
+      title('Phi vs orbit ')
+      ylabel('Phi (degrees)')
+      xlabel('Orbits (rev)')
+      hold on
+      color = 'k.-';
+   elseif (n_index == 2)
+      color = 'r.-';
+   elseif (n_index == 3)
+      color = 'b.-';
+   elseif (n_index == 4)
+      color = 'g.-';    
+   end
+      plot(nu,phi(n_index,:),color)
+end
+   hold off
+   savefig('output/proj2_phi',gcf,'pdf');
+   
 
 %% Task A:
 % %% Angular Velocity
